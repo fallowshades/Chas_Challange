1f78cbe46fe7396b38f23d1116b56f132d690c72
fallowShades <fallowshades@gmail.com>
2024-05-07 19:33:12 +0200

    fix redux-store

--- src/app/(dashboard)/auth/signup/page.jsx
+++ src/app/(dashboard)/auth/signup/page.jsx
@@ -1,22 +1,18 @@
-"use client"
-import React, { useRef } from 'react';
-import { useAppSelector, useAppDispatch, useAppStore } from '@/lib/hooks'; // Du behöver importera useAppSelector och useAppDispatch från dina Redux hooks
-import { initializeProduct } from '@/lib/features/user/userSlice'; // Du behöver importera initializeProduct från userSlice-filen
+'use client'
+import React, { useRef } from 'react'
+import { useAppSelector, useAppDispatch, useAppStore } from '@/lib/hooks' // Du behöver importera useAppSelector och useAppDispatch från dina Redux hooks
+import { initializeUser } from '@/lib/features/user/userSlice' // Du behöver importera initializeProduct från userSlice-filen
 
-
-const page = () => {
-  const store = useAppStore();
-  const initialized = useRef(false);
+const SignUp = ({ count }) => {
+  const store = useAppStore()
+  const initialized = useRef(false)
   if (!initialized.current) {
-    store.dispatch(initializeProduct(product));
-    initialized.current = true;
+    store.dispatch(initializeUser({ value: count }))
+    initialized.current = true
   }
-  const name = useAppSelector((state) => state.user.user);
-  const dispatch = useAppDispatch();
-  return (
-    <div>page {name}
-    </div>
-  )
+  const name = useAppSelector((state) => state.user.user)
+  const dispatch = useAppDispatch()
+  return <div>page {`name: ${name}`}</div>
 }
 
-export default page
+export default SignUp
--- src/app/storeProvider.jsx
+++ src/app/storeProvider.jsx
@@ -1,16 +1,16 @@
-"use client";
-import { useRef } from "react";
-import { Provider } from "react-redux";
-import { makeStore } from "../lib/store";
-import { initializeCount } from "../lib/features/user/userSlice";
+'use client'
+import { useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore } from '../lib/store'
+import { initializeUser } from '../lib/features/user/userSlice'
 
-export default function StoreProvider({ children }) {
-  const storeRef = useRef();
+export default function StoreProvider({ count, children }) {
+  const storeRef = useRef()
   if (!storeRef.current) {
     // Create the store instance the first time this renders
-    storeRef.current = makeStore();
-    storeRef.current.dispatch(initializeCount(count));
+    storeRef.current = makeStore()
+    storeRef.current.dispatch(initializeUser({ value: count }))
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return <Provider store={storeRef.current}>{children}</Provider>
 }
