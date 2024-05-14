af846faed01944cc14cea1c187dcb2edefe02968
fallowShades <fallowshades@gmail.com>
2024-05-14 08:18:44 +0200

    Fix loading import

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,30 @@
-import { dbConnect } from '@/utils/mongoose';
-import TaskCard from '@/components/TaskCard';
-import Task from '@/models/Task';
-import AllShadCn from '@/components/LibAllShadCn';
-import FlowbitComponents from '@/components/LibFlowbitComponents';
-import SemanticComponent from '@/components/LibSemantic';
-import Link from 'next/link';
+import { dbConnect } from '@/utils/mongoose'
+import TaskCard from '@/components/TaskCard'
+import Task from '@/models/Task'
+import AllShadCn from '@/components/LibAllShadCn'
+import FlowbitComponents from '@/components/LibFlowbitComponents'
+import SemanticComponent from '@/components/LibSemantic'
+import Link from 'next/link'
 
 export async function loadTasks() {
-  await dbConnect();
-  const tasks = await Task.find();
-  return tasks;
+  await dbConnect()
+  const tasks = await Task.find()
+  return tasks
 }
 
 export default async function HomePage() {
-  const tasks = await loadTasks();
+  const tasks = await loadTasks()
 
-  const convenientFindLibraryComponents = true;
+  const convenientFindLibraryComponents = true
 
   return (
     <div className='grid md:grid-cols-3 gap-2'>
       <Link href='/auth/signup'> signup </Link>
+      <Link href='/auth/login'> login </Link>
+      <Link href='/featured'> featured </Link>
+      <Link href='/places'> places </Link>
+      <Link href='/orders'> orders </Link>
+
       {tasks.map((task) => (
         <TaskCard task={task} key={task._id} />
       ))}
@@ -32,5 +37,5 @@ export default async function HomePage() {
         </div>
       )}
     </div>
-  );
+  )
 }
